97e49b3cb3c5e5126ebf952c97a83024d52fb96b
Alex Gordon <anything@fileability.net>
2012-10-13 16:27:27 +0100

    Ignoring the actual binary

--- source/net/net.hpp
+++ source/net/net.hpp
@@ -1,10 +1,14 @@
 #import <SFML/Network.hpp>
 
-
+/* Networking model
+    There are two sockets for each client
+        1) A TCP socket, for information that must be received (e.g sending new map tiles)
+        2) A UDP socket, for information that must be sent quickly, but may be dropped (e.g sending world updates)
+*/
 
 // void networkThreadMain(bool isServer) {
-    //
+    
     // sf::SocketUDP
-    //
+    
 // }
 
