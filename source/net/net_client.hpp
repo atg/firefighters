db29d22d8d1223b6fcd177b5194f4ec5bcf59469
Alex Gordon <anything@fileability.net>
2012-10-16 22:17:12 +0100

    Disabled aborting on malformed packet

--- source/net/net_client.hpp
+++ source/net/net_client.hpp
@@ -23,12 +23,54 @@ Thread 2:
 6   libsystem_c.dylib             	0x00007fff8d86fb75 thread_start + 13
 */
 
+/*
+
+Thread 0 Crashed:: Dispatch queue: com.apple.main-thread
+0   libsystem_kernel.dylib        	0x00007fff98600ce2 __pthread_kill + 10
+1   libsystem_c.dylib             	0x00007fff8d86e7d2 pthread_kill + 95
+2   libsystem_c.dylib             	0x00007fff8d85fa7a abort + 143
+3   hat.Firefighters              	0x0000000109f10d16 _ZL22clientReceiveGameStateRSt6vectorIcSaIcEE + 262
+4   hat.Firefighters              	0x0000000109f10bdd game_clientQuickUpdate(void*) + 29
+5   hat.Firefighters              	0x0000000109f1eb79 Invocation::invoke() + 25
+6   hat.Firefighters              	0x0000000109f155ea InvocationQueue::popAll() + 106
+7   hat.Firefighters              	0x0000000109f11872 main + 482
+8   hat.Firefighters              	0x0000000109eaf514 start + 52
+
+Thread 1:: Dispatch queue: com.apple.libdispatch-manager
+0   libsystem_kernel.dylib        	0x00007fff986017e6 kevent + 10
+1   libdispatch.dylib             	0x00007fff948d25be _dispatch_mgr_invoke + 923
+2   libdispatch.dylib             	0x00007fff948d114e _dispatch_mgr_thread + 54
+
+Thread 2:
+0   libsystem_kernel.dylib        	0x00007fff98601192 __workq_kernreturn + 10
+1   libsystem_c.dylib             	0x00007fff8d86e594 _pthread_wqthread + 758
+2   libsystem_c.dylib             	0x00007fff8d86fb85 start_wqthread + 13
+
+Thread 3:
+0   libsystem_kernel.dylib        	0x00007fff98601192 __workq_kernreturn + 10
+1   libsystem_c.dylib             	0x00007fff8d86e594 _pthread_wqthread + 758
+2   libsystem_c.dylib             	0x00007fff8d86fb85 start_wqthread + 13
+
+Thread 4:
+0   libsystem_kernel.dylib        	0x00007fff98600d7a __recvfrom + 10
+1   com.sfml.network              	0x000000010a2e7981 sf::SocketTCP::Receive(char*, unsigned long, unsigned long&) + 145
+2   com.sfml.network              	0x000000010a2e810b sf::SocketTCP::Receive(sf::Packet&) + 443
+3   hat.Firefighters              	0x0000000109f14a17 _ZL19clientNetworkThreadPv + 823
+4   com.sfml.system               	0x000000010a3aeaae sf::Thread::ThreadFunc(void*) + 30
+5   libsystem_c.dylib             	0x00007fff8d86c8bf _pthread_start + 335
+6   libsystem_c.dylib             	0x00007fff8d86fb75 thread_start + 13
+
+
+*/
+
 static void clientReceiveGameState(std::vector<char>& data) {
     ServerQuickUpdate u;
+    printf("Message is: %s\n", std::string(&data[0], data.size()).c_str());
     std::istringstream iss(std::string(&data[0], data.size()));
     if (!u.ParseFromIstream(&iss)) {
         fprintf(stderr, "Could not read quick update\n");
-        abort();
+        return;
+        // abort();
     }
     
     // For each player
