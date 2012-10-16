d551c27360b0144cd07c3a6030a5e26d2a542ece
Alex Gordon <anything@fileability.net>
2012-10-16 04:18:03 +0100

    Protobufs are working!

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 mkdir -p build/Firefighters.app/Contents/MacOS || true
 
 # Build .protos
-deps/protoc -Isource/net --cpp_out=source/net source/net
+deps/protobuf-2.4.1/src/protoc -Isource/net --cpp_out=source/net source/net/wire.proto
+mv source/net/wire.pb.cc source/net/wire.pb.cpp
 
-clang++ -std=c++0x -Fdeps/sfml-1.6/lib64 -framework SFML -framework sfml-audio -framework sfml-graphics -framework sfml-network -framework sfml-system -framework sfml-window -framework OpenGL -Ideps/sfml-1.6/lib64/SFML.framework/Headers -Isource -o build/Firefighters.app/Contents/MacOS/Firefighters source/*/*.cpp
+clang++ -std=c++0x -Ideps/protobuf-2.4.1/src -Ldeps/protobuf-2.4.1/src/.libs/ -lprotobuf -Fdeps/sfml-1.6/lib64 -framework SFML -framework sfml-audio -framework sfml-graphics -framework sfml-network -framework sfml-system -framework sfml-window -framework OpenGL -Ideps/sfml-1.6/lib64/SFML.framework/Headers -Isource -o build/Firefighters.app/Contents/MacOS/Firefighters source/*/*.cpp
 
--- source/net/net_client.hpp
+++ source/net/net_client.hpp
@@ -1,6 +1,7 @@
 #import "net/net.hpp"
 #import "world/map.hpp"
 #import "main/game.hpp"
+#import "wire.pb.h"
 
 static void clientReceiveGameState() {
     
